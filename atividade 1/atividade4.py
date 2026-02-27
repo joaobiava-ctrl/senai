10e8595ff1cbe7debd299da0bbf242f9fa538313
joaobiava-ctrl <joao_biava@estudante.sesisenai.org.br>
2026-02-27 19:43:57 +0000

    update

---
+++ atividade 1/atividade4.py
@@ -0,0 +1,10 @@
+aluno = input("isira o seu nome")
+nota1 = float(input("insira a primeira nota") )
+nota2 =  float(input("insira a segunda nota") )    
+media = (nota1 + nota2) /2
+print(" A media das notas é:", media)
+if media >= 7:
+    print("o aluno esta aprovado")
+else:
+     print("o aluno esta reprovado")
+     
---
+++ atividade 1/programa3.py
@@ -0,0 +1,4 @@
+nome = input("isira o nome do aluno)
+numero1 = int(input("insira a primeira nota") )
+numero2 =  int(input("insira a segunda nota") )    
+print("o usuario ",nome," obteve a soma dos dois valores de :",numero1 + numero2)                             
