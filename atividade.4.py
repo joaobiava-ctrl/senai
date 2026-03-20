bd0965fd14511b580686cf1d8f4845a763c91762
joaobiava-ctrl <joao_biava@estudante.sesisenai.org.br>
2026-03-20 20:19:37 +0000

    update

--- atividade.4.py
+++ atividade.4.py
@@ -1,12 +1,11 @@
-nome = float (input("digite seu nome: "))
+nome =  (input("digite seu nome: "))
 idade = float (input("digite sua idade: "))  
 
-if idade (>0
- print = ("idade invalida! tente novamente com um valor maior que 0"))
-if idade<120
- print = ("idade invalida! tente novamente com um valor menor que 120"))
+if idade > 120 or idade < 0:
+    print = ("idade invalida! tente novamente com um valor menor ou igual a  120.")
+else: 
+    dias_de_vida = idade * 365
+    print(f"olá {nome}, voce já viveu cerca de : {dias_de_vida}")
 
 
-dias_de_vida = idade * 365
-print("voce ja viveu: ",dias_de_vida, "de sua vida")
 
